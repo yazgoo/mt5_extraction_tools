dfb2c786e73c0b142a0eeb8c744b51e547d7fce3
yazgoo <yazgoo@github.com>
2014-09-21 23:08:08 +0200

    removing md5 from extract all

--- src/mt7/extract_all.sh
+++ src/mt7/extract_all.sh
@@ -1,14 +1,14 @@
 extract()
 {
-    md5=/tmp/$(md5sum ${f} | cut -d ' ' -f 1).md5
+    #md5=/tmp/$(md5sum ${f} | cut -d ' ' -f 1).md5
     file_path=$(echo $f | sed 's,/,_,g' | sed 's,\(.*\),/tmp/\1.png,')
-    if [ -e $md5 ]
-    then
-        cp $(cat $md5) $file_path
-    else
+    #if [ -e $md5 ]
+    #then
+    #    cp $(cat $md5) $file_path
+    #else
         MT7=$f blender --background --python ./mt7_loader.py 1>/dev/null 2>&1
-        echo $file_path > $md5
-    fi
+        #echo $file_path > $md5
+    #fi
     test -e $file_path
 }
 failed()
